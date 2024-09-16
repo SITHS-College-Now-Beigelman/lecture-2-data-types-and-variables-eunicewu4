08592d6fb896d88c77ba6c4a86118e7d17bd2916
eunicewu4 <eunicew6@nycstudents.net>
2024-09-16 16:33:29 -0400

    Lecture 2

---
+++ Wu_Eunice_Lecture_2.cpp
@@ -0,0 +1,24 @@
+// Eunice Wu
+// Lecture 2
+// September 16, 2024
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    int two = 2;
+    char six = s;
+    s=6; 
+    bool four = 4;
+    float threeptfive = 3.5;
+    string eighteen = 18;
+    double threeptnine = 3.9;
+
+    cout << "int:" << 
+
+
+
+    return 0;
+}
